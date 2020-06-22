e74e9f3cb84707af865879bf5d50b57201c6f88c
Kim Wysocki <kcwysocka@icloud.com>
2020-06-22 15:27:45 -0400

    set up basic server file and add default error handler

--- server/server.js
+++ server/server.js
@@ -1,5 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
 const app = express();
-const port = 3000;
+const PORT = 3000;
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Global error handler
+app.use((err, req, res, next) => {
+	const defaultError = {
+		log: "Express error handler caught unknown mmiddleware error",
+		status: 400,
+		message: { error: "An error occurred" },
+	};
+	const errorObj = { ...defaultError, err };
+	res.status(errorObj.status).send(errorObj);
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${PORT}...`);
+});
