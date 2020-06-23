8e92cb5f93a0fd46a05b57a670e559d73cfcc915
Kim Wysocki <kcwysocka@icloud.com>
2020-06-23 12:19:22 -0400

    handle merge conflicts

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const path = require("path");
-const router = require("./routes/routes");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const router = require('./routes/routes');
 const app = express();
 const PORT = 3000;
 
@@ -9,31 +9,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/", router);
+app.use('/', router);
 
-app.get("/", (req, res) => {
-	res.status(200).sendFile(path.resolve(__dirname, "../client/index.html"));
+app.get('/', (req, res) => {
+	res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
 // Global error handler
 app.use((err, req, res, next) => {
-<<<<<<< HEAD
 	const defaultError = {
-		log: "Express error handler caught unknown middleware error",
+		log: 'Express error handler caught unknown middleware error',
 		status: 400,
-		message: { error: "An error occurred" },
+		message: { error: `An error occurred, ${err}` },
 	};
 	const errorObj = { ...defaultError, err };
 	res.status(errorObj.status).send(errorObj);
-=======
-  const defaultError = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 400,
-    message: { error: `An error occurred, ${err}` },
-  };
-  const errorObj = { ...defaultError, err };
-  res.status(errorObj.status).send(errorObj);
->>>>>>> 91caed7bc41847caf41e1c03a84a88f46c96a81b
 });
 
 app.listen(PORT, () => {
