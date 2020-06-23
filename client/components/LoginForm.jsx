882bf13a591368db68760f51d6dfa279a7ea707a
Keiran Carpen <keirancarpen@gmail.com>
2020-06-23 10:22:14 -0400

    some more styling

--- client/components/LoginForm.jsx
+++ client/components/LoginForm.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 function LoginForm() {
-  const handleSubmit = () => {
+  const handleLogin = () => {
     const username = document.querySelector('#username');
     const password = document.querySelector('#password');
+    // post route supplying sign up info for verfiying user
+  };
+
+  const handleSignUp = () => {
+    const username = document.querySelector('#username');
+    const password = document.querySelector('#password');
+    // post route supplying sign up info to backend/db
   };
 
   return (
     <div id='login'>
+      <h1> AvocadoGrams! </h1>
+      <h3> Login </h3>
       <form>
         <div id='userLogin'>
           <label htmlFor='username'> Username </label>
@@ -19,6 +28,20 @@ function LoginForm() {
         </div>
       </form>
       <button id='submitLogin' type='submit'>
+        Submit
+      </button>
+      <h3> Sign Up </h3>
+      <form>
+        <div id='userSignUp'>
+          <label htmlFor='usernameSignUp'> Username </label>
+          <input id='usernameSignUp' name='usernameSignUp' placeholder='username' />
+        </div>
+        <div id='passSignUp'>
+          <label htmlFor='passwordSignUp'> Password </label>
+          <input id='passwordSignUp' name='passwordSignUp' placeholder='password' />
+        </div>
+      </form>
+      <button id='submitSignUp' type='submit'>
         Submit
       </button>
     </div>
--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(png|jpg|gif)/i,
+        test: /\.(png|jpg|gif)$/i,
         use: [
           {
             loader: 'url-loader',
