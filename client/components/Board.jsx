b18cd242ed20f0551713ff8b7812223273647f8f
Keiran Carpen <keirancarpen@gmail.com>
2020-06-23 18:17:25 -0400

    fixed some styling and added board position using hooks

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Board() {
-  return <div id='board'></div>;
+  const [boardPosition, setBoardPosition] = useState([]);
+  useEffect(() => {
+    function updateBoardPosition() {
+      const element = document.querySelector('#board');
+      const position = {
+        top: element.offsetTop,
+        left: element.offsetLeft,
+      };
+      setBoardPosition([position.top, position.left]);
+    }
+    if (!boardPosition.length) updateBoardPosition();
+    window.addEventListener('resize', updateBoardPosition);
+  }, []);
+
+  return (
+    <div id='board'>
+      {`top postion of board: ${boardPosition[0]} and left position of board: ${boardPosition[1]}`}
+    </div>
+  );
 }
 
 export default Board;
