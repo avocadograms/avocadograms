488fa436a9ed23f616dc852b9e5b8bbe909f1774
lpham598 <lpham598@gmail.com>
2020-06-23 15:42:28 -0400

    in progress oauth

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+//const oauthController = require('../controllers/oauthController');
 
 router.get('/getUser', userController.getUser, (req, res) => {
     res.status(200).json(res.locals.user)
