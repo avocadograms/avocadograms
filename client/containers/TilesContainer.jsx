7a910d321a7594527c8624892b5d9d9d3ad72027
Keiran Carpen <keirancarpen@gmail.com>
2020-06-24 16:43:45 -0400

    added an obj of letters and freqs and randomized 20 letters weighted by freqs

--- client/containers/TilesContainer.jsx
+++ client/containers/TilesContainer.jsx
@@ -1,31 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Tiles from '../components/Tiles.jsx';
+import letterList, { letterFrequencies } from '../constants/letterPool';
 
 function TilesContainer() {
-  const [letters, setLetters] = useState([
-    'A',
-    'B',
-    'D',
-    'R',
-    'T',
-    'H',
-    'J',
-    'W',
-    'A',
-    'Q',
-    'O',
-    'A',
-    'B',
-    'D',
-    'R',
-    'T',
-    'H',
-    'J',
-    'W',
-    'A',
-    'Q',
-    'O',
-  ]);
+  const [letters, setLetters] = useState([]);
+
+  useEffect(() => {
+    const cache = { ...letterFrequencies };
+    let randomLetters = [];
+    while (randomLetters.length < 20) {
+      let randomNum = Math.floor(Math.random() * (letterList.length - 1));
+      if (cache[letterList[randomNum]] !== 0) {
+        randomLetters.push(letterList[randomNum]);
+        cache[letterList[randomNum]] -= 1;
+      }
+    }
+    setLetters(randomLetters);
+  }, []);
 
   const tiles = letters.map((letter, i) => <Tiles letter={letter} key={`${letters}${i}`} />);
   return <div id='tilesContainer'> {tiles} </div>;
