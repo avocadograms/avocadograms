8a7896b263c25d4ec15de718e516e44b23594226
Keiran Carpen <keirancarpen@gmail.com>
2020-06-24 12:25:39 -0400

    edited the flex-wrap property of tiles container

--- client/containers/TilesContainer.jsx
+++ client/containers/TilesContainer.jsx
@@ -2,7 +2,30 @@ import React, { useState } from 'react';
 import Tiles from '../components/Tiles.jsx';
 
 function TilesContainer() {
-  const [letters, setLetters] = useState(['A', 'B', 'D', 'R', 'T', 'H', 'J', 'W', 'A', 'Q', 'O']);
+  const [letters, setLetters] = useState([
+    'A',
+    'B',
+    'D',
+    'R',
+    'T',
+    'H',
+    'J',
+    'W',
+    'A',
+    'Q',
+    'O',
+    'A',
+    'B',
+    'D',
+    'R',
+    'T',
+    'H',
+    'J',
+    'W',
+    'A',
+    'Q',
+    'O',
+  ]);
 
   const tiles = letters.map((letter, i) => <Tiles letter={letter} key={`${letters}${i}`} />);
   return <div id='tilesContainer'> {tiles} </div>;
