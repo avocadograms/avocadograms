3aba8db1b54d37adb3e38a310417a054209dd60f
Kim Wysocki <kcwysocka@icloud.com>
2020-06-25 15:13:30 -0400

    style changes (background colors)

--- client/containers/GameContainer.jsx
+++ client/containers/GameContainer.jsx
@@ -3,37 +3,42 @@ import TilesTracker from '../components/TilesTracker.jsx';
 import GameplayFunctions from '../components/GameplayFunctions.jsx';
 import Board from '../components/Board.jsx';
 import TilesContainer from './TilesContainer.jsx';
+import PlayerTab from '../components/PlayerTab.jsx';
 import letterList, { letterFrequencies } from '../constants/letterPool';
 
 function GameContainer({ setWordsArray }) {
-  const [letters, setLetters] = useState([]);
-  const [letterPool, setLetterPool] = useState(letterFrequencies);
+	const [letters, setLetters] = useState([]);
+	const [letterPool, setLetterPool] = useState(letterFrequencies);
 
-  useEffect(() => {
-    let randomLetters = [];
-    while (randomLetters.length < 20) {
-      let randomNum = Math.floor(Math.random() * (letterList.length - 1));
-      const letter = letterList[randomNum];
-      if (letterPool[letter] !== 0) {
-        randomLetters.push(letter);
-        const obj = {};
-        obj[letter] = letterPool[letter] - 1;
-        setLetterPool({ ...letterPool, ...obj });
-      }
-    }
-    setLetters(randomLetters);
-  }, []);
+	useEffect(() => {
+		let randomLetters = [];
+		while (randomLetters.length < 20) {
+			let randomNum = Math.floor(Math.random() * (letterList.length - 1));
+			const letter = letterList[randomNum];
+			if (letterPool[letter] !== 0) {
+				randomLetters.push(letter);
+				const obj = {};
+				obj[letter] = letterPool[letter] - 1;
+				setLetterPool({ ...letterPool, ...obj });
+			}
+		}
+		setLetters(randomLetters);
+	}, []);
 
-  return (
-    <div id='gameContainer'>
-      <div>
-        <TilesTracker />
-        <GameplayFunctions setWordsArray={setWordsArray} />
-      </div>
-      <Board />
-      <TilesContainer letters={letters} />
-    </div>
-  );
+	return (
+		<div id="gameContainer">
+			<div>
+				<TilesTracker />
+				<GameplayFunctions setWordsArray={setWordsArray} />
+			</div>
+			<Board />
+			<TilesContainer letters={letters} />
+
+			<div id="players-div">
+				<PlayerTab username={'Keiran'} />
+			</div>
+		</div>
+	);
 }
 
 export default GameContainer;
